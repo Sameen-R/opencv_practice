b500499898601bb6ee79eafb3c9bdbba83f40aac
Sarthak Agrawal <sarthavader@gmail.com>
2021-01-18 19:25:57 -0600

    added blob detection

--- experiment_values.py
+++ experiment_values.py
@@ -10,7 +10,7 @@ In the imread() line, use the IMAGE_NAME variable
 Then, run the program and press 'm' to predict the path
 '''
 #To find the predicted path, run the program and press 'm'
-IMAGE_NAME = 'obj1.jpg'
+IMAGE_NAME = 'obj3.jpg'
 path_options = [(479.89423077, 325.51442308), (619.56692913, 251.41994751), (408.97231834, 343.41176471), (523.62751678, 226.62080537)]
 
 def nothing(x):
@@ -25,55 +25,77 @@ cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)
 cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
 cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
 
-while True:
-    # reading image
-    img = cv2.imread(IMAGE_NAME, -1)
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+# reading image
+img = cv2.imread(IMAGE_NAME, -1)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    # what to do with each vid frame
-    l_h = cv2.getTrackbarPos("LH", "Tracking")
-    l_s = cv2.getTrackbarPos("LS", "Tracking")
-    l_v = cv2.getTrackbarPos("LV", "Tracking")
-    h_h = cv2.getTrackbarPos("UH", "Tracking")
-    h_s = cv2.getTrackbarPos("US", "Tracking")
-    h_v = cv2.getTrackbarPos("UV", "Tracking")
+# what to do with each vid frame
+l_h = cv2.getTrackbarPos("LH", "Tracking")
+l_s = cv2.getTrackbarPos("LS", "Tracking")
+l_v = cv2.getTrackbarPos("LV", "Tracking")
+h_h = cv2.getTrackbarPos("UH", "Tracking")
+h_s = cv2.getTrackbarPos("US", "Tracking")
+h_v = cv2.getTrackbarPos("UV", "Tracking")
 
-    lower = np.array([mean_l_h, mean_l_s, mean_l_v])
-    upper = np.array([mean_u_h, mean_u_s, mean_u_v])
-    mask = cv2.inRange(img, lower, upper)
+# lower = np.array([mean_l_h, mean_l_s, mean_l_v])
+# upper = np.array([mean_u_h, mean_u_s, mean_u_v])
 
-    kernel = np.ones((3,3), np.uint8)
-    opening_img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+lower = np.array([l_h, l_s, l_v])
+upper = np.array([h_h, h_s, h_v])
 
-    points = cv2.findNonZero(mask)
-    if cv2.countNonZero(mask)>0:
-        avg = np.mean(points, axis=0)
-    else:
-        avg = None
+mask = cv2.inRange(img, lower, upper)
 
-    # displaying
-    cv2.imshow('processed image', mask)
-    k = cv2.waitKey(1)
-    if k==ord('q'):
-        break
-    elif k==ord('p'):
-        print(f'LH:{l_h} LS:{l_s} LV:{l_v} >>> UH:{h_h} US:{h_s} UV:{h_v}')
-    elif k==ord('s'):
-        print(f'shape: {mask.shape}')
-    elif k==ord('m'):
-        min_dist = -1
-        index = -1
-        for i in range(0,4):
-            if distance.euclidean(path_options[i],avg)<min_dist or min_dist==-1:
-                min_dist = distance.euclidean(path_options[i],avg)
-                index = i
-        print(f'Path # {index+1}')
+kernel = np.ones((3, 3), np.uint8)
+opening_img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+
+points = cv2.findNonZero(mask)
+if cv2.countNonZero(mask) > 0:
+    avg = np.mean(points, axis=0)
+else:
+    avg = None
+
+# displaying
+params = cv2.SimpleBlobDetector_Params()
+params.filterByColor = False
+params.filterByArea = False
+params.filterByCircularity = False
+params.filterByInertia = False
+params.filterByConvexity = False
+detector = cv2.SimpleBlobDetector_create(params)
+keypoints = detector.detect(mask)
+sumX = 0
+sumY = 0
+count = 0
+for kp in keypoints:
+    count+=1
+    sumX+=kp.pt[0]
+    sumY+=kp.pt[1]
+
+print(count)
+
+print(str(sumX/3) + " " + str(sumY/3))
+im_with_keypoints = cv2.drawKeypoints(mask, keypoints, np.array([]), (0, 0, 255),
+                                      cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv2.imshow('processed image', im_with_keypoints)
+k = cv2.waitKey(0)
+if k == ord('p'):
+    print(f'LH:{l_h} LS:{l_s} LV:{l_v} >>> UH:{h_h} US:{h_s} UV:{h_v}')
+elif k == ord('s'):
+    print(f'shape: {mask.shape}')
+elif k == ord('m'):
+    min_dist = -1
+    index = -1
+    for i in range(0, 4):
+        if distance.euclidean(path_options[i], avg) < min_dist or min_dist == -1:
+            min_dist = distance.euclidean(path_options[i], avg)
+            index = i
+    print(f'Path # {index + 1}')
 
 
 cv2.destroyAllWindows()
 
 '''
-Obj1: mean=480.52324037 327.4873838  new mean: 479.89423077 325.51442308 
+Obj1: mean=480.52324037 327.4873838  new mean: 479.89423077 325.51442308 blob mean=465.78973388671875 305.0594075520833
 Lh:0
 Ls:224
 Lv:237
@@ -81,7 +103,7 @@ Uh:40
 Us:255
 Uv:255
 
-Obj2: mean=615.16331658 250.83417085  new mean=619.56692913 251.41994751
+Obj2: mean=615.16331658 250.83417085  new mean=619.56692913 251.41994751 blob mean=571.5816853841146 245.081787109375
 Lh:0
 Ls:230
 Lv:245
@@ -89,7 +111,7 @@ Uh:105
 Us:255
 Uv:255
 
-Obj3: mean=409.30617284 342.99012346  new mean=408.97231834 343.41176471
+Obj3: mean=409.30617284 342.99012346  new mean=408.97231834 343.41176471 blob mean=563.9024556477865 433.1412862141927
 Lh:0
 Ls:232
 Lv:242
